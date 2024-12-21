854f242e96d4a57e7b1d7dc78fee219d3e48586b
EmanuelaCaetano <aparecidacaetano75@gmail.com>
2024-12-21 20:09:25 -0300

    componente NossoDiferencial revisado

--- correcao_vercel/SITE-main/jr-fea-rp/src/components/NossoDiferencial/index.tsx
+++ correcao_vercel/SITE-main/jr-fea-rp/src/components/NossoDiferencial/index.tsx
@@ -5,17 +5,16 @@ export const NossoDiferencial = () => {
   const cardsData = [
     {
       frontContent: { title: 'Preço Acessível', image: 'precoBaixoBranco.png' },
-      backContent: { image: 'precoBaixoVermelho.png', text: 'A Júnior FEA-RP se configura entre as melhores empresas de consultoria empresarial júnior do Brasil, com custos abaixos do mercado. Dessa forma, entregamos projetos de alta qualidade com preços mais acessíveis.' },
+      backContent: { title: 'Preço Acessível', image: 'precoBaixoVermelho.png', text: 'A Júnior FEA-RP se configura entre as melhores empresas de consultoria empresarial júnior do Brasil, com custos abaixos do mercado. Dessa forma, entregamos projetos de alta qualidade com preços mais acessíveis.' },
     },
     {
-      frontContent: { title: 'Desenvolvimento', image: 'desenvolvimentoBranco.png'  },
-      backContent: { image: 'desenvolvimentoVermelho.png', text: 'Ajudamos nossos clientes com conhecimentos técnicos e teóricos sobre gestão, visando o apoio ao empreendedorismo e à inovação.' },
+      frontContent: { title: 'Desenvolvimento', image: 'desenvolvimentoBranco.png' },
+      backContent: { title: 'Desenvolvimento', image: 'desenvolvimentoVermelho.png', text: 'Ajudamos nossos clientes com conhecimentos técnicos e teóricos sobre gestão, visando o apoio ao empreendedorismo e à inovação.' },
     },
     {
-      frontContent: { title: 'Incentivo', image: 'incentivoBranco.png'  },
-      backContent: { image: 'incentivoVermelho.png', text: 'Acreditamos no potencial de cada empresa e cada cliente. Além de produzirmos soluções, fazemos com que eles também acreditem em seus sonhos e busquem realizá-los.' },
+      frontContent: { title: 'Incentivo', image: 'incentivoBranco.png' },
+      backContent: { title: 'Incentivo', image: 'incentivoVermelho.png', text: 'Acreditamos no potencial de cada empresa e cada cliente. Além de produzirmos soluções, fazemos com que eles também acreditem em seus sonhos e busquem realizá-los.' },
     },
-    // Adicione mais cartões conforme necessário
   ];
     return(
         <>
