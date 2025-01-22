02fe2180cc96a2b50cbdb4bd1ca6b1a4c7a9a258
PedroGabrielGuimaraesFernandes <pedrogabriel0412@gmail.com>
2025-01-22 11:56:51 -0300

    Ajustes na responsividade da Pagina Principal

--- correcao_vercel/SITE-main/jr-fea-rp/src/components/IntroducaoHomePage/index.tsx
+++ correcao_vercel/SITE-main/jr-fea-rp/src/components/IntroducaoHomePage/index.tsx
@@ -12,32 +12,44 @@ interface IntroducaoProps {
   context: string;
 }
 
-const IntroducaoHomePage: React.FC<IntroducaoProps> = ({ imgPath,subTitle, context}) => {
+const IntroducaoHomePage: React.FC<IntroducaoProps> = ({ imgPath, subTitle, context }) => {
   return (
     <>
-<div className="bg-corPrimaria w-full pb-4 rounded-b-lg shadow-lg h-[800px]">
-            <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 1 }}
-                className="w-full h-[770px] bg-no-repeat bg-cover bg-center flex justify-center drop-shadow-lg rounded-b-2xl"
-                style={{ backgroundImage: `url(${imgPath})` }}
-            >
-  <div className="w-full h-[770px] pt-16 bg-gradient-to-t from-corPrimaria/20 to-black/85 flex flex-col items-center rounded-b-2xl">
-    <div className="w-full h-1/6 flex flex-col items-center justify-center mt-40">
-      <LetterPullup words="De sonhos a projetos: O impacto é a nossa marca!" />
-    </div>
-    <div className="flex flex-col items-center">
-      <h3 className="text-white font-light pt-4 text-2xl">{subTitle}</h3>
-    </div>
-    <div className="flex-1 flex flex-col items-center pt-4 px-8">
-      <p className="text-white text-xl font-light">{context}</p>
-    </div>
-
-  </div>
-  </motion.div>
-</div>
+      <div className="bg-corPrimaria w-full pb-4 shadow-lg h-[800px]">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+          className="w-full h-[770px] bg-no-repeat bg-cover bg-center flex justify-center drop-shadow-lg rounded-b-2xl"
+          style={{ backgroundImage: `url(${imgPath})` }}
+        >
+          <div className="w-full h-[770px] pt-16 bg-gradient-to-t from-corPrimaria/20 to-black/85 flex flex-col items-center rounded-b-2xl">
+            {/* Texto principal */}
+            <div className="w-full h-1/6 flex flex-col items-center justify-center mt-40">
+              <LetterPullup
+                className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold sm:font-semibold md:font-medium lg:font-normal xl:font-light"
+                words="De sonhos a projetos: O impacto é a nossa marca!"
+              />
+            </div>
+            {/* Subtítulo */}
+            <div className="flex flex-col items-center">
+              <h3 className="text-white pt-4 text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl font-bold sm:font-semibold md:font-medium lg:font-normal xl:font-light">
+                {subTitle}
+              </h3>
+            </div>
+            {/* Contexto */}
+            <div className="flex-1 flex flex-col items-center pt-4 px-8">
+              <p className="text-white text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl font-bold sm:font-semibold md:font-medium lg:font-normal xl:font-light">
+                {context}
+              </p>
+            </div>
+          </div>
+        </motion.div>
+      </div>
     </>
-  )}
+  );
+};
+
+
 
 export default IntroducaoHomePage;
--- correcao_vercel/SITE-main/jr-fea-rp/src/components/sobreJRFEA-Home/index.tsx
+++ correcao_vercel/SITE-main/jr-fea-rp/src/components/sobreJRFEA-Home/index.tsx
@@ -7,23 +7,23 @@ import { AnimacaoEscrita } from '@/components/ANIMACOES/AnimacaoEscrita';
 export default function SobreJRFEA() {
     return (
         <>
-            <div className="bg-corPrimaria text-white drop-shadow-lg py-16 px-4 sm:px-6 md:px-12 lg:px-28 xl:px-40 2xl:px-60 flex flex-col justify-center min-h-screen mt-60">
+            <div className="bg-corPrimaria text-white drop-shadow-lg py-16 px-4 sm:px-6 md:px-12 lg:px-28 xl:px-40 2xl:px-60 flex flex-col justify-center min-h-screen ">
                 <AnimacaoEscrita loop={true} texts={["Sobre JR FEA RP", "A maior consultoria do interior"]} />
                 <div className="flex flex-col lg:flex-row justify-between items-center gap-8 sm:gap-10 lg:gap-16 pt-8">
                     {/* Texto */}
-                    <div className="lg:w-1/2 text-center lg:text-left space-y-6 font-bold text-sm sm:text-base md:text-lg lg:text-xl drop-shadow-lg">
+                    <div className="order-2 lg:order-1 lg:w-1/2 text-center lg:text-left space-y-6 font-bold text-sm sm:text-base md:text-lg lg:text-xl drop-shadow-lg">
                         <FadeText text="Fundada em 1992, a Júnior FEA-RP é uma empresa júnior formada por alunos da FEA USP Ribeirão Preto. Nossos projetos têm como finalidade transformar a ideia dos nossos clientes em realidades de sucesso e contribuir para a vivência empresarial." />
                         <FadeText text="Nos situamos na melhor universidade da América Latina, nossa equipe recebe treinamentos constantes de parceiros como McKinsey & Company e são acompanhados por professores doutores da USP especializados no ramo." />
                         <FadeText text="Com isso, procuramos construir as melhores soluções e estratégias de reconhecimento, posicionamento no mercado e inovação." />
                     </div>
 
                     {/* Imagem */}
-                    <div className="lg:w-1/2 max-w-full flex justify-center">
+                    <div className="order-1 lg:order-2 lg:w-1/2 max-w-full flex justify-center">
                         <Image
                             src="/sobreHomePage.jpg"
                             alt="Foto da equipe JR FEA RP"
-                            width={600}
-                            height={600}
+                            width={800}
+                            height={800}
                             className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl drop-shadow-lg rounded-lg hover:scale-105 transition-transform duration-300"
                         />
                     </div>
